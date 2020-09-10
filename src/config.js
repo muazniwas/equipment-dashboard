04ceedf68ab13b23658d3055920653e2e380baa2
Muaz Niwas <muaz.niwas@gmail.com>
2020-09-10 23:32:35 +0530

    Add proxy to allow CORS

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const ASSETS_API_URL = "http://ivivaanywhere.ivivacloud.com/api/Asset/Asset/All";
+export const ASSETS_API_URL = "/api/Asset/Asset/All";
 export const ASSETS_API_KEY = "SC:demo:64a9aa122143a5db";
